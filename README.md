1db1a0035090a0b8b6957bbde14e1dc7a476af45
sulamitaivaonv <sulamita.ivanov@gmail.com>
2023-12-19 14:18:53 -0500

    uploaded entire proj

---
+++ README.md
@@ -0,0 +1,6 @@
+![](expandBTN.gif)
+
+Expand div on click with JavaScript. 
+
+Clone this repo
+`git clone https://github.com/sulamitaivanov/expand-button`
---
+++ main.js
@@ -0,0 +1,13 @@
+var content = document .getElementById("content");
+var button = document .getElementById("show-more");
+
+button.onclick = function(){
+  if(content.className == "open") {
+    
+    content.className = "";
+    button.innerHTML = "Show More";
+  } else{
+    content.className = "open";
+    button.innerHTML = "Show Less";
+  }
+};
